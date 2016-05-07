ede1ae97dc5467127a3d8be018456c60a3df8eb7
devjet <devjet@users.noreply.github.com>
2016-05-08 00:28:10 +0300

    initial pm2 test

---
+++ app.js
@@ -0,0 +1,13 @@
+//simple express app
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello, World!');
+});
+
+var server = app.listen(3000, function() {
+
+    console.log('Server listening at http://%s:%s');
+});
